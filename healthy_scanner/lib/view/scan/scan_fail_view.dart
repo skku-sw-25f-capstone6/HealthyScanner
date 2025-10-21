f4806e79d077a3d1442a3581abcc9e22389334db
hongseekim <songhee0617@gmail.com>
2025-10-22 01:11:10 +0900

    feat: 스캔 실패 화면 구현

---
+++ healthy_scanner/lib/view/scan/scan_fail_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:healthy_scanner/theme/app_colors.dart';
+import 'package:healthy_scanner/theme/theme_extensions.dart';
+import 'package:healthy_scanner/component/bottom_button.dart';
+
+class ScanFailView extends StatelessWidget {
+  const ScanFailView({
+    super.key,
+    this.onRetry,
+  });
+
+  final VoidCallback? onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              const Image(
+                image: AssetImage('assets/icons/ic_fail.png'),
+                width: 128,
+                height: 128,
+              ),
+              const SizedBox(height: 17),
+              // TODO: 발생한 에러에 따라 분기 처리
+              Text(
+                '오류가 발생했어요',
+                textAlign: TextAlign.center,
+                style: context.title2Medium.copyWith(
+                  color: AppColors.staticBlack,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                '사진을 다시 촬영해 주세요',
+                textAlign: TextAlign.center,
+                style: context.caption2Regular.copyWith(
+                  color: AppColors.staticBlack,
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+
+      // 하단 버튼
+      bottomNavigationBar: SafeArea(
+        minimum: const EdgeInsets.fromLTRB(15, 0, 15, 10),
+        child: BottomButton(
+          text: '다시 시도하기',
+          onPressed: () {
+            // TODO: ScanReadyView로 이동
+          },
+        ),
+      ),
+    );
+  }
+}
