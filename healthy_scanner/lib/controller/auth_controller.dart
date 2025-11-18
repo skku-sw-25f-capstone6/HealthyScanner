f8036ab8bed4f0b136088649d81f53de235cf0ed
hongseekim <songhee0617@gmail.com>
2025-11-18 14:27:32 +0900

    feat: iOS용 로그인URL 추가

--- healthy_scanner/lib/controller/auth_controller.dart
+++ healthy_scanner/lib/controller/auth_controller.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:healthy_scanner/controller/navigation_controller.dart';
 
 class AuthController extends GetxController {
-  /// FastAPI 로컬 개발용 URL (Android 에뮬레이터는 10.0.2.2 사용)
-  static const String BACKEND_LOGIN_URL =
-      "http://10.0.2.2:8000/auth/kakao/login";
+  /// 플랫폼별 FastAPI 로컬 개발용 URL
+  static String get backendLoginUrl {
+    if (Platform.isAndroid) {
+      return "http://10.0.2.2:8000/auth/kakao/login";
+    }
+
+    if (Platform.isIOS) {
+      return "http://127.0.0.1:8000/auth/kakao/login";
+    }
+
+    return "http://127.0.0.1:8000/auth/kakao/login";
+  }
 
   final nav = Get.find<NavigationController>();
 
@@ -14,7 +25,7 @@ class AuthController extends GetxController {
   Future<void> startKakaoLogin() async {
     // 서버는 JSON이 아니라 즉시 카카오 login 페이지로 Redirect하므로
     // 그대로 WebView에서 URL을 열면 된다.
-    nav.goToKakaoWebView(BACKEND_LOGIN_URL);
+    nav.goToKakaoWebView(backendLoginUrl);
   }
 
   /// ----------------------------------------------------------
